750e213dee6d21371ac0303c21cb9ab492a13973
dertyu <dertyu@DESKTOP-A36HV7B>
2021-12-13 11:33:18 +0000

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private object textBox2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 OutroForm = new Form2();
+            OutroForm.ShowDialog();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        { }
+        static void SendUdp(int PortId, string IpConf, int PortIdAberta, byte[] data)
+            {
+                using (UdpClient c = new UdpClient(PortId))
+                    c.Send(data, data.Length, IpConf, PortIdAberta);
+            }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        { }
+        public string Recebe()
+            {
+                UdpClient receivingUdpClient = new UdpClient(Convert.ToInt32(textBox2));
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+
+
+
+                    Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+
+                    return returnData.ToString();
+
+                }
+                catch (Exception e)
+                {
+
+                    return e.ToString();
+                }
+
+            }
+        
+    }
+}
---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox3.Text);
+            listBox2.Items.Add(DateTime.Now.Date.ToShortDateString());
+            listBox2.Items.Add(DateTime.Now.TimeOfDay.ToString());
+        }
+    }
+}
